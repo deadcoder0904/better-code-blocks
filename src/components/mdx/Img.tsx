057fcce49fbdab9620840d775273c152745faede
Akshay Kadam <akshaykadam0904@gmail.com>
2021-05-25 15:53:04 +0530

    Add CSS Grid Layout support

--- src/components/mdx/Img.tsx
+++ src/components/mdx/Img.tsx
@@ -1,9 +1,11 @@
-// import Image, { ImageProps } from 'next/image'
+import Image, { ImageProps } from 'next/image'
 
-// export const Img = ({ className = '', ...props }: ImageProps) => (
-// 	<Image className={`${className}`} layout="responsive" {...props} />
-// )
-
-export const Img = ({ className = 'full-bleed', ...props }) => (
-	<img className={`${className}`} {...props} />
+export const Img = ({ className = '', ...props }: ImageProps) => (
+	<div className="unset-img full-bleed">
+		<Image className={`${className} custom-img`} layout="fill" {...props} />
+	</div>
 )
+
+// export const Img = ({ className = 'full-bleed', ...props }) => (
+// 	<img className={`${className}`} {...props} />
+// )
